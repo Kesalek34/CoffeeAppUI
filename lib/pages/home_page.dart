fdd62a16ac6401168c23fb2165109467b3d173a2
Kesaobaka Lekaote <lekaote45@gmail.com>
2025-09-18 22:12:42 +0200

    still need to finish of cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; // Add this import for GoogleFonts
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +12,10 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         leading: Icon(Icons.menu),
         actions: [
-            Padding(
-                padding: const EdgeInsets.only(right:20.0),
-                child: Icon(Icons.person),
-
-                ),
-         
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.person),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -28,9 +27,48 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('Hello, Coffee!'),
-        ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              'Find the best coffee for you',
+              style: GoogleFonts.bebasNeue(
+                 fontSize: 56,
+              ), 
+             
+            ),
+          ),
+             SizedBox(height: 25),
+
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Find your coffee...',
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade600)),
+                  
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600),
+                  ),
+                     
+                ),
+               ),
+             ),
+        
+
+     SizedBox(height: 25),
+
+     Expanded(
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        
+        children: [
+       
+          
+      ],          
       ),
-    );
+    )    ],
+      ),
+    );  
   }
 }
